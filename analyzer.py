2f63eeca8c8405209269950c5934ddbb537ce182
St3fan27 <balasas0327@yahoo.com>
2026-04-04 18:55:50 +0300

    Updated the TechAnalyzer to access 5 sites at once

--- analyzer.py
+++ analyzer.py
@@ -1,10 +1,67 @@
 import asyncio
-from playwright.async_api import async_playwright
+from playwright.async_api import async_playwright, Playwright, Browser
 from bs4 import BeautifulSoup
 
 class TechAnalyzer:
     
     def __init__(self, domains):
         self.domains = domains
-        
-        
+        self.results = {}
+        self.browser: Browser | None = None
+
+    async def start_browser(self, p: Playwright):
+        self.webkit = p.webkit
+        self.browser = await self.webkit.launch()
+
+    async def run_page(self, semaphore, page_url, index):
+        async with semaphore:
+            # I have defined the protocols that need to be tried
+            protocols = ["https://", "http://"]
+            context = None
+            # I use try to not stop scanning other sites if browser/system errors occur
+            try:
+                # I used a user_agent to hide the fact that the script is a bot
+                context = await self.browser.new_context(ignore_https_errors=True, user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+                page = await context.new_page()
+                # I didn't load images, css and media to increase speed
+                await page.route("**/*", lambda route: route.abort() 
+                                 if route.request.resource_type in ["image", "media", "font", "stylesheet"] 
+                                 else route.continue_())
+
+                ok = False
+                for protocol in protocols:
+                    url = f"{protocol}{page_url}"
+                    print(f"{index} Scanning: {url}")
+                    # I use try to avoid stopping the entire script when a site goes down
+                    try:
+                        # I wait 30 seconds to see if the site works if not we throw an error
+                        await page.goto(url, timeout=30000)
+                        # I wait for JavaScript to load
+                        await page.wait_for_timeout(2000)
+                        await page.screenshot(path=f"tests/{index}.png")
+                        print(f"{index} Ok")
+                        ok = True
+                        break
+
+                    except Exception as e:
+                        print(f"{index} Error: {e}")
+                
+                # I write down the sites that couldn't run
+                if not ok:
+                    print(f"{index}, {url} Error ")
+                    with open("links_error.txt", "a", encoding="utf-8") as f:
+                        f.write(f"{page_url} No protocol succeeded\n")
+
+            # Error in case there is not enough ram or there is a problem with Playwright
+            except Exception as e:
+                print(f"{index}, {page_url} System Error : {e}")
+                with open("links_error.txt", "a", encoding="utf-8") as f:
+                    f.write(f"{page_url} Playwright Error\n")
+
+            finally:
+                if context:
+                    await context.close()
+
+    async def close_browser(self):
+        if self.browser:
+           await self.browser.close()
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import asyncio
 import pandas as pd
+from playwright.async_api import async_playwright, Playwright
 import json
 from analyzer import TechAnalyzer
 
 async def main():
     df = pd.read_parquet("part-00000-66e0628d-2c7f-425a-8f5b-738bcd6bf198-c000.snappy.parquet")
     domains = df["root_domain"].unique().tolist()
-
     analyzer = TechAnalyzer(domains)
+    async with async_playwright() as p:
+        await analyzer.start_browser(p)
+        semaphore = asyncio.Semaphore(5)
+        await asyncio.gather(*(analyzer.run_page(semaphore, domains[i], i + 1) for i in range(len(domains))))
+        await analyzer.close_browser()
 
 if __name__ == "__main__":
     asyncio.run(main())
